b8cfdd264a66d6af83572dde257487d3d79fb391
cabaioloco <80918424+cabaioloco@users.noreply.github.com>
2022-04-04 20:54:46 +0200

    Add files via upload

--- Unidad_9/src/layaoutAnterior/Main.java
+++ Unidad_9/src/layaoutAnterior/Main.java
@@ -1,34 +1,32 @@
-package layoutCompleto;
+package layaoutAnterior;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.FlowPane;
-import javafx.stage.Stage;
 
-public class Main extends Application{
-	
+public class Main extends Application {
+
 	private BorderPane rootLayout;
 
+	@Override
 	public void start(Stage primaryStage) {
 		try {
-
+			// Carga el diseño del archivo FXML en la variable rootLayout
 			FXMLLoader loader = new FXMLLoader();
-			loader.setLocation(Main.class.getResource("MainLayaout.fxml"));
+			loader.setLocation(Main.class.getResource("MainLayouts.fxml"));
 			rootLayout = (BorderPane) loader.load();
-
+			// Mostramos la escena del BorderPane de la variable rootLayot
 			Scene scene = new Scene(rootLayout);
-			scene.getStylesheets().add("layoutCompleto/application.css");
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
 }
-
